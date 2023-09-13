f546efbd1d64546c28204313289e2bcd21dd806f
adwaithrrajesh <adwaithrrajesh@hotmail.com>
2023-09-13 14:24:02 +0530

    Linear Search Learned

---
+++ Searching/linearSearch.js
@@ -0,0 +1,11 @@
+function linearSearch(userInp){
+    const array = [1,2,4,3,2,4]
+    for(let i=0;i<array.length;i++){
+        if(array[i] == userInp){
+            return console.log(`Value found at array postion ${i}`);
+        }
+    }
+    return console.log('unable to find the value')
+}
+
+linearSearch(2)
